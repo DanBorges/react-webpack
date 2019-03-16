7a6daf29e3d868b403b9675524c90e237a6848d2
Daniel Borges <lemebor91@yahoo.com.br>
2019-03-16 18:38:24 -0300

    add source map

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
-	// devtool:'source-map',
+	devtool:'source-map',
 	entry: [
 		'react-hot-loader/patch',
 		'webpack-dev-server/client?http://localhost:3000',

6a5d591529591ea7f29c04008d0c66236a0cfeda
Daniel Borges <lemebor91@yahoo.com.br>
2019-03-16 18:35:26 -0300

    removendo node_modules

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import createReactClass from 'create-react-class'
 
 const App = createReactClass({
      render: function () {
-        return <h1>Titulo 245 </h1>
+        return <h1>Titulo 255 </h1>
      }
 })
 
